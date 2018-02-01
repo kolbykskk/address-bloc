566a82ba32ed9fe770fe4907f622cf6867f75c61
Kolby <kolbykalafut@gmail.com>
2018-02-01 09:58:12 -0500

    Assignment 5 completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -13,7 +13,8 @@ def main_menu
   puts "2 - Create an entry"
   puts "3 - Search for an entry"
   puts "4 - Import entries from a CSV"
-  puts "5 - Exit"
+  puts "5 - View Entry Number"
+  puts "6 - Exit"
   print "Enter your selection: "
 
   selection = gets.to_i
@@ -35,6 +36,10 @@ def main_menu
        read_csv
        main_menu
      when 5
+       system 'clear'
+       view_entry
+       main_menu
+     when 6
        puts "Good-bye!"
        exit(0)
      else
@@ -76,6 +81,17 @@ def main_menu
   end
 
   def read_csv
+  end
+
+  def view_entry
+    print 'Enter entry number to view: '
+    entry_number = gets.chomp.to_i
+    if entry_number >= address_book.entries.count
+      puts "#{entry_number} is not valid"
+      view_entry
+    else
+      puts address_book.entries[entry_number]
+    end
   end
 
   def entry_submenu(entry)
